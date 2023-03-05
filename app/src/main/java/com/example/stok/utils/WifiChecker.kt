305fd7fdd828569a728e361ae3b30cddc249a295
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-03-05 22:54:20 +0100

    [WIP]

--- app/src/main/java/com/example/stok/utils/WifiChecker.kt
+++ app/src/main/java/com/example/stok/utils/WifiChecker.kt
@@ -4,13 +4,19 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.wifi.WifiManager
 import androidx.core.content.ContextCompat.getSystemService
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.stok.databinding.ActivityMainBinding
 
 class WifiChecker {
     companion object {
-        fun checkWifi () {
+        fun checkWifi (ctx:Context) {
 
-            val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager;
-            val wifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager;
+            val connectivityManager = ctx.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager;
+            val wifiManager = ctx.getSystemService(Context.WIFI_SERVICE) as WifiManager;
 
             val network = connectivityManager.activeNetwork;
             val capabilities = connectivityManager.getNetworkCapabilities(network);
@@ -24,7 +30,7 @@ class WifiChecker {
                 // TODO toast try to connect
                 if ( !wifiManager.isWifiEnabled ) {
                     AlertDialog
-                        .Builder(this)
+                        .Builder(ctx)
                         .setTitle("Wi-Fi is disabled")
                         .setMessage("Application requires internet connection.")
                         .setPositiveButton("Enable Wi-Fi") { _, _ ->
